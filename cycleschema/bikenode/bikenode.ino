e3269b01c2e7875d064df3471b0d988aa8689d49
TIm Gilchrist <timbo@TIms-MacBook-Pro.local>
2017-01-14 18:13:58 +0800

    First light

--- cycleschema/bikenode/bikenode.ino
+++ cycleschema/bikenode/bikenode.ino
@@ -1,9 +1,73 @@
+
+#include <NMEAGPS.h>
+#include "GPSPort.h"
+#include <RHReliableDatagram.h>
+#include <RH_RF69.h>
+#include <SPI.h>
+
+
+#define LED_RED 10
+#define LED_GREEN 12
+
+#define CHURCH_NODE 1 //The base node at camp ΒΣLLIGΣRΣΠCΣ
+#define BIKE_NODE 2 //Which network node are we? 2..n for bikes 1..n-1
+#define BELLIGERENCE "ΒΣLLIGΣRΣΠCΣ" //handy string to send
+
+// Singleton instance of the radio driver
+
+RH_RF69 driver(8, 7); // Adafruit Feather 32u4
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, BIKE_NODE);
+
+
 void setup() {
-  // put your setup code here, to run once:
- "ΒΣLLIGΣRΣΠCΣ"
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, LOW);
+  if (!manager.init()) {
+    digitalWrite(LED_RED, HIGH);
+    delay(400);
+    digitalWrite(LED_RED, LOW); 
+    delay(100);
+  }
+   digitalWrite(LED_GREEN, LOW);
+   digitalWrite(LED_RED, HIGH);
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+
+  // If you are using a high power RF69, you *must* set a Tx power in the
+  // range 14 to 20 like this:
+  // driver.setTxPower(14);
 }
 
+uint8_t data[] = BELLIGERENCE;
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_GREEN, HIGH); 
+  // Send a message to manager_server
+  digitalWrite(LED_GREEN, HIGH);
+  if (manager.sendtoWait(data, sizeof(data), CHURCH_NODE))
+  {
+    // Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;   
+    /*
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    {
+       
+    }
+    else
+    {
+        
+    }
+    */
+  }
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_RED, LOW); 
+  delay(300);
+  digitalWrite(LED_RED, HIGH); 
+ 
 }
---
+++ cycleschema/churchnode/churchnode.ino
@@ -0,0 +1,76 @@
+#include <NMEAGPS.h>
+#include "GPSPort.h"
+#include <RHReliableDatagram.h>
+#include <RH_RF69.h>
+#include <SPI.h>
+
+#define LED_RED 10
+#define LED_GREEN 12
+
+#define CHURCH_NODE 1 //The base node at camp ΒΣLLIGΣRΣΠCΣ
+#define BIKE_NODE 2 //Which network node are we? 2..n for bikes 1..n-1
+#undef BIKE_NODE
+#define BELLIGERENCE F("ΒΣLLIGΣRΣΠCΣ") //handy string to send
+
+// Singleton instance of the radio driver
+
+RH_RF69 driver(8, 7); // Adafruit Feather 32u4
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, CHURCH_NODE);
+
+// Dont put this on the stack:
+uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+
+
+void setup() {
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, LOW);
+  
+  Serial.begin(9600);
+  while (!Serial) 
+    ;
+  Serial.println("Serial, Yo.");
+  if (!manager.init())
+    Serial.println("init failed");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+
+  // If you are using a high power RF69, you *must* set a Tx power in the
+  // range 14 to 20 like this:
+  // driver.setTxPower(14);
+   digitalWrite(LED_GREEN, LOW);
+   digitalWrite(LED_RED, HIGH);
+}
+
+int packetCounter = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   if (manager.available())
+  {
+     
+    // Serial.println(packetCounter);
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
+    {
+      Serial.print("message# ");
+      Serial.print(packetCounter);
+      Serial.print(". got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+
+      // Send a reply back to the originator client
+      //if (!manager.sendtoWait(data, sizeof(data), from))
+      //  Serial.println("sendtoWait failed");
+      packetCounter++;
+    }
+  }
+  
+
+}
